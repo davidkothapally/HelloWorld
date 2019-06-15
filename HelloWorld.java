2742eacf4aabcf3a09f0be9db398e591bff2b4f2
davidkothapally <51866673+davidkothapally@users.noreply.github.com>
2019-06-15 18:58:44 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+
+public class HelloWorld
+{
+   public static void main(String[] args){
+       int a = 4;
+       double b = 7.324;
+       String c = "Lets check the calculation if it works. 4 x 7.324 = ";
+       
+       b = b * a;
+       
+       System.out.println(c+b);
+    }
+}
